25df1b0701abc25cd08c5e124b520f50f55deb06
Rikin Marfatia <rikinm10@gmail.com>
2015-08-17 05:13:41 -0400

    messing around with jQuery color animations

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
+var colors = ["#4747FF", "#E84343", "#19D175"];
+var index = 0;
+
 $(function () {
-    var colors = [""]
-    $('.rikin')
+    animateName();
 });
+
+function animateName() {
+    $('.rikin').animate({color: colors[(index++) % colors.length]}, 3000, animateName);
+}
